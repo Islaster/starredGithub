c099aef8f3dbb379245a85897c8875ccdaa74308
Islaster <52678788+Islaster@users.noreply.github.com>
2023-09-04 18:57:39 -0700

    init push

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const { Octokit, App } = require("octokit");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+const GITHUB_API_URL = "https://api.github.com/search/repositories";
+require("dotenv").config();
+
+async function fetchMostStarredRepos(startDate, endDate) {
+  try {
+    const response = await axios.get(GITHUB_API_URL, {
+      params: {
+        q: `created:${startDate}..${endDate} stars:>1000`, // You can adjust the search query as needed
+        sort: "stars",
+        order: "desc",
+      },
+      headers: {
+        Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+    });
+    const { items } = response.data;
+
+    // Display information about the top 10 most starred repositories
+    for (let i = 0; i < 10; i++) {
+      const repo = items[i];
+      console.log(`#${i + 1}: ${repo.name} - ${repo.stargazers_count} stars`);
+    }
+  } catch (error) {
+    console.error("Error fetching data from GitHub API:", error);
+  }
+}
+
+// Use inquirer to prompt for dates
+async function getUserInput() {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "startDate",
+      message: "Enter start date (YYYY-MM-DD):",
+    },
+    {
+      type: "input",
+      name: "endDate",
+      message: "Enter end date (YYYY-MM-DD):",
+    },
+  ]);
+
+  const { startDate, endDate } = answers;
+  return { startDate, endDate };
+}
+
+async function main() {
+  const { startDate, endDate } = await getUserInput();
+  await fetchMostStarredRepos(startDate, endDate);
+}
+
+main(); // Call the main function to start the program
---
+++ readme.md
@@ -0,0 +1,29 @@
+    # Starred Github
+
+Search github for the top 10 most starrred repos.
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+  - [Configuration](#configuration)
+- [Running the App](#running-the-app)
+
+## Prerequisites
+
+1. Node.js
+
+## Getting Started
+
+clone repo from top of page
+
+### Installation
+
+1. Open code editor
+   open cloned repo inside of code editor
+
+2. open terminal
+   go to cloned repo
+   enter command npm i or npm install
+   enter command node index.js
